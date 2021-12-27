7f7fe859d3aae6d54ec05972680a250fe5ea3518
jarb29 <jarb29@gmail.com>
2021-12-27 16:58:54 -0300

    adding text

--- src/components/_external-pages/landing/LandingHero.js
+++ src/components/_external-pages/landing/LandingHero.js
@@ -82,8 +82,8 @@ export default function LandingHero() {
             </motion.div>
             <motion.div variants={varFadeInRight}>
               <Typography variant="h6" sx={{ color: 'common.white' }}>Cocción artesanal al humo, 
-              envasada al vacío para que puedas disfrutar de las bondades y el realce del sabor que esta ofrece 
-              en la comodidad de su casa, sin complicaciones. 
+              envasada al vacío para que puedas disfrutar de las bondades y el realce del sabor que esta ofrece, 
+              en la comodidad de su casa y sin complicaciones. 
               </Typography>
             </motion.div>
             <motion.div variants={varFadeInRight}>
